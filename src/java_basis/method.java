283867264f05ce39e8c003820ac7534e146ef9fa
leedongab <kaid37@naver.com>
2020-05-08 17:40:29 +0900

    method private, public

--- src/java_basis/method.java
+++ src/java_basis/method.java
@@ -1,5 +1,26 @@
 package java_basis;
 
 public class method {
+	public method() { //생성자
+		// TODO Auto-generated constructor stub
+	}
+	public void name() {
 
+	}
+	public static void name2() {
+
+	}
+	private void name3() {
+
+	}
+	private int intmethod() {
+		return 0;
+
+	}
+	public int intmethod2() {
+		int a = 0 ;
+		a = a+100;
+
+		return a;
+	}
 }
