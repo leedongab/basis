f5f0244fddf5891693d37f4adc08f26fb5828213
leedongab <kadi37@hanmail.net>
2020-05-09 16:53:23 +0900

    메소드 보이드형과 리턴형의 차이

--- src/java_basis/method.java
+++ src/java_basis/method.java
@@ -4,23 +4,24 @@ public class method {
 	public method() { //생성자
 		// TODO Auto-generated constructor stub
 	}
-	public void name() {
+	public void int_void(int b) {
+		//바로 실행되는 메소드 //변수에 담아 사용할 필요가 없다.
+		// 매개변수로 자료형이 int b 라는 변수로 활동 한다.
+		b=10;
 
+		b += 10;
 	}
-	public static void name2() {
 
-	}
-	private void name3() {
+	public int int_return(int b) {
+		//바로 실행되는 메소드 //변수에 담아 사용할 필요가 없다.
+		// 매개변수로 자료형이 int b 라는 변수로 활동 한다.
+		b=10;
 
-	}
-	private int intmethod() {
-		return 0;
+		b += 10;
 
+		return b;
 	}
-	public int intmethod2() {
-		int a = 0 ;
-		a = a+100;
 
-		return a;
-	}
+
+
 }
